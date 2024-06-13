eab4314a031b0f9395bb8a8e6ae36b9fb8d11140
Atharvack <91490274+Atharvack@users.noreply.github.com>
2024-06-13 00:57:29 -0400

    Update Basics.py

--- Basics.py
+++ Basics.py
@@ -5,6 +5,7 @@ print("Hello World!")
 print("Would you be kind and tell me your name?")
 name = input()
 print("Hello",name,'It is great to meet you.')
+print() #a blank line
 print("Can you please let me know your age?")
 age = int(input())
 print("Hi", name ,'Age:' + str(age) ,'It was great getting to know you better. You will be ',age+2,'after next two years')
