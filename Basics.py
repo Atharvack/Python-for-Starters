ec31077a3a5709ef9ea538e6516aee5066fb4ad6
Atharvack <91490274+Atharvack@users.noreply.github.com>
2024-06-13 00:50:16 -0400

    Add files via upload
    Understanding the basics of the Variables , Data types and operators

---
+++ Basics.py
@@ -0,0 +1,10 @@
+#Understanding the Basics of Python.
+#Variables, Operations , and Data types.
+
+print("Hello World!")
+print("Would you be kind and tell me your name?")
+name = input()
+print("Hello",name,'It is great to meet you.')
+print("Can you please let me know your age?")
+age = int(input())
+print("Hi", name ,'Age:' + str(age) ,'It was great getting to know you better. You will be ',age+2,'after next two years')
