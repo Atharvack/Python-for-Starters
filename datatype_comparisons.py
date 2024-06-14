c587e44f5533258b9222a157d587a1dd63f649dc
Atharvack <91490274+Atharvack@users.noreply.github.com>
2024-06-14 14:07:23 -0400

    Add files via upload

---
+++ datatype_comparisons.py
@@ -0,0 +1,7 @@
+#Input function and boolean comparisons
+#Input default intake is string 
+print("Please enter the same number twice:")
+str_num=input()
+num= float(input())
+#Since the comparison operator can compare between two different datatypes 
+print("Comparison between the str_num==num:",str_num==num,"Since even though the numbers entered are same the datatypes they are stored in are completely different")
